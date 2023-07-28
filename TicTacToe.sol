e6ac119d7cda21701bf9f4851485de444d0a2bb2
Olivier Scherrer <pode.fr@gmail.com>
2023-07-28 13:57:31 +0200

    Add empty TicTacToe contract

---
+++ TicTacToe.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// This is a Smart Contract for playing Tic Tac Toe against another player.
+// For simplicity, Each Smart Contract is a new game.
+contract TicTacToeContract {
+
+}
